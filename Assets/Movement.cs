81632f1f7ba259b33fb33227dbc0c19beb792752
AugustUu <auusitalo8059@student.pps.net>
2023-10-01 14:47:34 -0700

    Baisc Movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -8,11 +8,12 @@ public class Movement : MonoBehaviour
 
 
     public CharacterController controller;
+    public new Transform transform;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
+    public float playerSpeed = 2.0f;
+
+    private float rotation = 0;
 
     private void Start()
     {
@@ -26,22 +27,11 @@ public class Movement : MonoBehaviour
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        //transform.rotation.y += 1;
 
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * playerSpeed;
+        controller.Move(move);
 
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
     }
 
 }
