4aaa1e01008276cdd51c6087ef451a2887960be1
ILove8288 <Ilove8288@student.pps.net>
2023-10-01 15:04:37 -0700

    Time implementation and camera changes

---
+++ Assets/TimeCycle.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimeCycle : MonoBehaviour
+{
+    [SerializeField]
+    public float minutes = 0.0f;
+    [SerializeField]
+    public float hours = 0.0f;
+    [SerializeField]
+    public int days = 0;
+    [SerializeField]
+    TextMeshProUGUI dayGUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Sets sun rotation right as the game starts
+        transform.rotation = Quaternion.Euler((float)((hours * 60 + minutes) / 1440.0 * 360 - 90), 0, 0);
+        // Runs addTime after 1 second every second
+        InvokeRepeating("addTime", 1.0f, 1.0f);
+        if (minutes < 10)
+        {
+            dayGUI.text = hours + " : 0" + minutes;
+        }
+        else
+        {
+            dayGUI.text = hours + " : " + minutes;
+        }
+        if (hours < 10)
+        {
+            dayGUI.text = "0" + dayGUI.text;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void addTime()
+    {
+        // Original Godot code
+        // (float)Mathf.LerpAngle(rotation.X,(float)((hours* 60 + minutes) / 1440.0 * 2*Math.PI),delta);
+
+        minutes += 5;
+        recalculateTime();
+        transform.rotation = Quaternion.Euler((float)((hours * 60 + minutes) / 1440.0 * 360 - 90), 0, 0);
+        if (minutes < 10)
+        {
+            dayGUI.text = hours + " : 0" + minutes;
+        }
+        else
+        {
+            dayGUI.text = hours + " : " + minutes;
+        }
+        if (hours < 10)
+        {
+            dayGUI.text = "0" + dayGUI.text;
+        }
+
+    }
+    void recalculateTime()
+    {
+
+        if (minutes >= 60)
+        {
+            minutes -= 60;
+            hours += 1;
+        }
+
+        if (hours >= 24)
+        {
+            hours = 0;
+            days += 1;
+        }
+    }
+}
