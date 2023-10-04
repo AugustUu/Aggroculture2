bada7c238d450700b307429ab8073d1164caae78
ILove8288 <Ilove8288@student.pps.net>
2023-10-04 10:46:22 -0700

    finished flashlight

--- Assets/Flashlight.cs
+++ Assets/Flashlight.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
-public class script : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
+    Light flashlight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        flashlight = GetComponent<Light>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Flashlight")){
+            flashlight.enabled = !flashlight.enabled;
+        }
     }
 }
