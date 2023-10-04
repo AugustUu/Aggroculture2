36143f10c054ba41394e352f0010c41db12b762a
tschmidt8320 <tschmidt8320@student.pps.net>
2023-10-04 12:01:24 -0700

    inventory code grid finder mouse thing works
    it works yay at 10:37 in tutorial right now !!???!?!?!!?!?!?!?!!?!?!?!??

--- Assets/ItemGrid.cs
+++ Assets/ItemGrid.cs
@@ -9,13 +9,17 @@ public class ItemGrid : MonoBehaviour
     private void Start(){
         rect_transform = GetComponent<RectTransform>();
     }
-
-    const float tile_size_width = 16;
-    const float tile_size_height = 16;
+    public float tile_size_width = 24;
+    public float tile_size_height = 24;
 
     Vector2 grid_pos = new Vector2();
-
-    /*public Vector2Int GetGridPos(Vector2 mouse_pos){
-        grid_pos.x = mouse_pos.x - rect_transform.position.y
-    }*/
+    Vector2Int tile_grid_pos = new Vector2Int();
+    
+    public Vector2Int GetGridPos(Vector2 mouse_pos){
+        grid_pos.x = mouse_pos.x - rect_transform.position.x;
+        grid_pos.y = rect_transform.position.y - mouse_pos.y;
+        tile_grid_pos.x = (int) (grid_pos.x / tile_size_width);
+        tile_grid_pos.y = (int) (grid_pos.y / tile_size_width);
+        return tile_grid_pos;
+    }
 }
