4e78a5e0e3338bb9b6de6b7720c508e8e94858b8
tschmidt8320 <tschmidt8320@student.pps.net>
2023-10-10 11:41:00 -0700

    forgot to save stuff
    whoops

--- Assets/InvItem.cs
+++ Assets/InvItem.cs
@@ -6,4 +6,8 @@ public class InvItem : MonoBehaviour
 {
     public int item_width = 1;
     public int item_height = 1;
+
+    public void Awake(){
+        // ItemGrid.scaled_tile_size this will scale item
+    }
 }
