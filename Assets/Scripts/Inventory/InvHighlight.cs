fcd0a4706ca791e30ee8c657e38a7a673c49486d
AugustUu <auusitalo8059@student.pps.net>
2023-12-04 11:41:27 -0800

    merged inventory

---
+++ Assets/Scripts/Inventory/InvHighlight.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvHighlight : MonoBehaviour
+{
+    [SerializeField] RectTransform highlighter;
+
+
+    public void SetTop(bool b){
+        if(b){
+            highlighter.SetAsLastSibling();
+        }
+        else{
+            highlighter.SetAsFirstSibling();
+        }
+    }
+    public void SetVisible(bool b)
+    {
+        highlighter.gameObject.SetActive(b);
+    }
+
+    public void SetSize(InvItem held_item)
+    {
+        highlighter.sizeDelta = new Vector2(
+            held_item.item_data.width * ItemGrid.scaled_tile_size, 
+            held_item.item_data.height * ItemGrid.scaled_tile_size
+        );
+    }
+
+    public void SetPosition(ItemGrid target_grid, InvItem held_item)
+    {
+        highlighter.localPosition = target_grid.GetItemPos(held_item, held_item.grid_pos);
+    }
+
+    public void SetPosition(ItemGrid target_grid, InvItem held_item, Vector2Int mouse_grid_pos)
+    {
+        highlighter.localPosition = target_grid.GetItemPos(held_item, mouse_grid_pos);
+    }
+
+    public void SetParent(ItemGrid target_grid)
+    {
+        if(target_grid == null){ return; }
+        highlighter.SetParent(target_grid.GetComponent<RectTransform>());
+    }
+}
