a0ff05e03cd6904e50cc692b80073bda46cbaa05
tschmidt8320 <tschmidt8320@student.pps.net>
2023-12-08 11:40:54 -0800

    started the thing
    startwd working on the inventory toggleanizer
    also rotated player by 5 degrees

---
+++ Assets/Scripts/Inventory/InvManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class InvManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Inventory"))
+        {
+        }
+    }
+
+    static void ToggleInv(){
+        // SetInvActive(!activeSelf)
+    }
+    static void SetInvActive(bool active){
+
+    }
+}
