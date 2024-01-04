c6d1789c758fb0e440dff774ff6f369dbbc9c217
AugustUu <auusitalo8059@student.pps.net>
2024-01-04 11:37:45 -0800

    adding "ai"

--- Assets/Scripts/FarmlandScript.cs
+++ Assets/Scripts/FarmlandScript.cs
@@ -8,14 +8,10 @@ public class FarmlandScript : MonoBehaviour
     public int plant = 0;
     public int growth = 0;
 
-    private MeshFilter mesh;
 
-    public Mesh[] stages;
 
     void Start()
     {
-        mesh = this.GetComponent<MeshFilter>();
-        mesh.mesh = stages[0];
         InvokeRepeating("grow", 0.0f, 1.0f);
     }
     void OnDrawGizmos()
@@ -26,8 +22,6 @@ public class FarmlandScript : MonoBehaviour
     void grow()
     {
         growth += 1;
-
-        mesh.mesh = stages[growth%4];
     }
 
 
---
+++ Assets/Scripts/MobScript.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MobScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public Transform target;
+
+    void Update(){
+        
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position; 
+    
+        Vector3 forward = new Vector3();
+        float rotation = Mathf.Deg2Rad *transform.rotation.eulerAngles.y;
+
+        forward.x = (float)(Math.Sin(rotation));
+        forward.z = (float)(Math.Cos(rotation));
+
+        Ray ray = new Ray(this.transform.position, forward);    
+        RaycastHit hit_object;
+        if (Physics.Raycast(ray, out hit_object)){
+            Debug.DrawRay(ray.origin, ray.direction * hit_object.distance, Color.red, 1f);
+        }else{
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.blue, 0.01f);
+        }
+
+        rotation += 0.78539816339f;
+        
+        forward.x = (float)(Math.Sin(rotation));
+        forward.z = (float)(Math.Cos(rotation));
+
+        ray = new Ray(this.transform.position, forward);    
+        if (Physics.Raycast(ray, out hit_object)){
+            Debug.DrawRay(ray.origin, ray.direction * hit_object.distance, Color.red, 1f);
+        }else{
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.blue, 0.01f);
+        }
+
+        rotation -= 0.78539816339f*2;
+        
+        forward.x = (float)(Math.Sin(rotation));
+        forward.z = (float)(Math.Cos(rotation));
+
+        ray = new Ray(this.transform.position, forward);    
+        if (Physics.Raycast(ray, out hit_object)){
+            Debug.DrawRay(ray.origin, ray.direction * hit_object.distance, Color.red, 1f);
+        }else{
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.blue, 0.01f);
+        }
+
+        
+    }
+}
