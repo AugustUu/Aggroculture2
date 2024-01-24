ca592f781ed7558ee311c822348fb93d083c7f25
ILove8288 <Ilove8288@student.pps.net>
2024-01-24 11:34:46 -0800

    xp pickup + levels

--- Assets/Scripts/Chracter/xpSystem.cs
+++ Assets/Scripts/Chracter/xpSystem.cs
@@ -1,23 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class XpSystem : MonoBehaviour
 {
     public Slider experience_points;
+    public TextMeshProUGUI display;
 
     private static Slider experience_points_rf;
     static int exp;
+    int exp_req = 100;
+    int levels = 1;
     // Start is called before the first frame update
     void Start()
     {
         experience_points_rf = experience_points;
         setExp(0);
+        display.text = levels.ToString();
     }
 
     void Update(){
         //changeExp(1);
+        if(exp >= exp_req){
+            exp = 0;
+            levels += 1;
+            exp_req = (int)(exp_req *1.2);
+            experience_points.maxValue = exp_req;
+            display.text = levels.ToString();
+        }
     }
 
     public static void setExp(int xp){
