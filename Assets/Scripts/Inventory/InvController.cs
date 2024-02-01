ce353f15a8249a4837da1c55cc7add73576929f0
tschmidt8320 <tschmidt8320@student.pps.net>
2024-02-01 10:45:37 -0800

    "merged" inventory branch
    updated scripts, changed inventory parents around, added equip highlighter rect

--- Assets/Scripts/Inventory/InvController.cs
+++ Assets/Scripts/Inventory/InvController.cs
@@ -6,12 +6,12 @@ using UnityEngine;
 
 public class InvController : MonoBehaviour
 {
-    [HideInInspector] private ItemGrid selected_item_grid;
+    public ItemGrid selected_item_grid;
     public ItemGrid Selected_item_grid { 
         get => selected_item_grid; 
         set {
             selected_item_grid = value;
-            inv_highlighter.SetParent(selected_item_grid);
+            inv_highlighter.SetParent(0, selected_item_grid);
         }
     }
 
@@ -19,23 +19,21 @@ public class InvController : MonoBehaviour
         get => selected_item; 
         set {
             selected_item = value; 
-            if(value == null){
-                inv_highlighter.SetTop(true);
-            }
-            else{
-                inv_highlighter.SetTop(false);
+            if(Selected_item_grid != null){
+                HandleHighlight(false);
             }
         }
     }
 
     private InvItem selected_item;
+    public InvItem equipped_item;
 
     private Vector3 drag_offset;
     private Vector2Int tile_offset;
     InvItem overlap_item;
     RectTransform rt_held;
     RectTransform rt_new;
-    ItemGrid origin_grid;
+    [SerializeField] ItemGrid origin_grid;
     Vector2Int origin_pos;
 
     [SerializeField] List<ItemData> items;
@@ -43,6 +41,9 @@ public class InvController : MonoBehaviour
     [SerializeField] Transform canvas_transform;
     [SerializeField] ItemGrid main_grid;
     [SerializeField] GameObject inv_parent;
+
+    public static float main_canvas_tile_size;
+    public static float main_scaled_tile_size;
 
     InvHighlight inv_highlighter;
 
@@ -54,46 +55,50 @@ public class InvController : MonoBehaviour
     {
         DragItemIcon();
 
-        if(Input.GetKeyDown(KeyCode.Q) && Selected_item == null)
+        if(Input.GetKeyDown(KeyCode.Q) && selected_item == null)
         {
-            Selected_item = GenerateRandomItem();
+            Selected_item = GenerateItem(Random.Range(0, items.Count));
             rt_held = rt_new;
             rt_new = null;
         }
 
         if(Input.GetKeyDown(KeyCode.W)){
-            InsertItem(GenerateRandomItem());
+            InsertItem(GenerateItem(Random.Range(0, items.Count)));
         }
 
         if(selected_item_grid != null){
 
-            HandleHighlight();
+            HandleHighlight(true);
 
             if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0)){
 
                 Vector2Int mouse_pos = selected_item_grid.GetGridPos(Input.mousePosition);
 
-                if (Input.GetMouseButtonDown(0) && Selected_item == null)
+                if (Input.GetMouseButtonDown(0) && selected_item == null)
                 {
+                    if(selected_item_grid.GetItem(mouse_pos) != equipped_item){
+                        Selected_item = selected_item_grid.PickUpItem(mouse_pos);
+                        if (selected_item != null)
+                        {
+                            selected_item.Rescale(main_canvas_tile_size);
+                            rt_held = selected_item.GetComponent<RectTransform>();
 
-                    Selected_item = selected_item_grid.PickUpItem(mouse_pos);
-                    if (Selected_item != null)
-                    {
-                        rt_held = Selected_item.GetComponent<RectTransform>();
-                        drag_offset = Input.mousePosition - rt_held.position;
-                        
-                        tile_offset = selected_item.grid_pos - mouse_pos;
-                        
-                        origin_pos = selected_item.grid_pos;
+                            float offset_scale = main_canvas_tile_size / selected_item_grid.canvas_tile_size;
+                            drag_offset = (rt_held.position - Input.mousePosition) * offset_scale;
+                            
+                            tile_offset = selected_item.grid_pos - mouse_pos;
+                            
+                            origin_pos = selected_item.grid_pos;
 
-                        rt_held.SetParent(inv_parent.transform);
-                        origin_grid = selected_item_grid;
+                            rt_held.SetParent(inv_parent.transform);
+                            origin_grid = selected_item_grid;
+                        }
                     }
+                    
                 }
-                if (Input.GetMouseButtonUp(0) && Selected_item != null)
+                if (Input.GetMouseButtonUp(0) && selected_item != null)
                 {
-                    if(selected_item_grid.PlaceItem(Selected_item, mouse_pos + tile_offset, ref overlap_item)){
-                        Selected_item = null;
+                    if(selected_item_grid.PlaceItem(selected_item, mouse_pos + tile_offset, ref overlap_item)){
                         tile_offset = Vector2Int.zero;
                     }
                     else{
@@ -101,17 +106,27 @@ public class InvController : MonoBehaviour
                             Debug.Log(overlap_item);
                             overlap_item = null;
                         }
-                        Debug.Log("goop wee");
+                        Debug.Log("item dropped on other item");
                         ReturnItem();
+                        if(equipped_item == selected_item){
+                            inv_highlighter.SetParent(1, origin_grid);
+                            inv_highlighter.SetPosition(1, origin_grid, selected_item);
+                        }
                     }
+                    
+                    Selected_item = null;
                 }
+            }
+            if (Input.GetMouseButtonDown(1) && selected_item == null){
+                EquipItem();
             }
         }
         else{
-            inv_highlighter.SetVisible(false);
-            if (Input.GetMouseButtonUp(0) && Selected_item != null){
+            inv_highlighter.SetVisible(0, false);
+            if (Input.GetMouseButtonUp(0) && selected_item != null){
                 ReturnItem();
-                Debug.Log("asdf beans");
+                Selected_item = null;
+                Debug.Log("item dropped outside inv");
             }
         }
         
@@ -119,42 +134,41 @@ public class InvController : MonoBehaviour
 
     InvItem highlighted_item;
     Vector2Int old_pos;
-    private void HandleHighlight()
+    private void HandleHighlight(bool check_mouse)
     {
         Vector2Int mouse_grid_pos = selected_item_grid.GetGridPos(Input.mousePosition) + tile_offset;
-        if(old_pos == mouse_grid_pos){ return; }
+        if(check_mouse){
+            if(old_pos == mouse_grid_pos){ return; }
+        }
         old_pos = mouse_grid_pos;
         
-        if(Selected_item == null){
+        if(selected_item == null){
             highlighted_item = selected_item_grid.GetItem(mouse_grid_pos);
-            if(highlighted_item != null){
-                inv_highlighter.SetTop(true);
-                inv_highlighter.SetVisible(true);
-                inv_highlighter.SetSize(highlighted_item);
-                inv_highlighter.SetParent(selected_item_grid);
-                inv_highlighter.SetPosition(selected_item_grid, highlighted_item);
+            if(highlighted_item != null && highlighted_item != equipped_item){
+                inv_highlighter.SetSize(0, highlighted_item, selected_item_grid);
+                inv_highlighter.SetParent(0, selected_item_grid);
+                inv_highlighter.SetPosition(0, selected_item_grid, highlighted_item);
+                inv_highlighter.SetVisible(0, true);
             }
             else{
-                inv_highlighter.SetVisible(false);
+                inv_highlighter.SetVisible(0, false);
             }
         }
         else{
-            inv_highlighter.SetTop(false);
-            inv_highlighter.SetVisible(selected_item_grid.BoundsCheck(mouse_grid_pos, Selected_item.item_data.width, Selected_item.item_data.height));
-            inv_highlighter.SetSize(Selected_item);
-            inv_highlighter.SetParent(selected_item_grid);
-            inv_highlighter.SetPosition(selected_item_grid, Selected_item, mouse_grid_pos);
+            inv_highlighter.SetSize(0, selected_item, selected_item_grid);
+            inv_highlighter.SetParent(0, selected_item_grid);
+            inv_highlighter.SetPosition(0, selected_item_grid, selected_item, mouse_grid_pos);
+            inv_highlighter.SetVisible(0, selected_item_grid.BoundsCheck(mouse_grid_pos, selected_item.item_data.width, selected_item.item_data.height));
         }
     }
-
-    private InvItem GenerateRandomItem()
+    
+    private InvItem GenerateItem(int item_ID)
     {
         InvItem inv_item = Instantiate(item_prefab).GetComponent<InvItem>();
         rt_new = inv_item.GetComponent<RectTransform>();
         rt_new.SetParent(canvas_transform);
 
-        int selected_item_ID = Random.Range(0, items.Count);
-        inv_item.Set(items[selected_item_ID]);
+        inv_item.Set(items[item_ID]);
         return inv_item;
     }
 
@@ -165,21 +179,48 @@ public class InvController : MonoBehaviour
         }
         else{
             Destroy(inserting_item.gameObject);
+            Debug.Log("found no space for inserting item, debug destroying item");
         }
+    }
+
+    public void InsertItemID(int item_ID){
+        InsertItem(GenerateItem(item_ID));
     }
 
     private void DragItemIcon()
     {
-        if (Selected_item != null)
+        if (selected_item != null)
         {
-            rt_held.position = Input.mousePosition - drag_offset;
+            rt_held.position = Input.mousePosition + drag_offset;
         }
     }
 
     private void ReturnItem()
     {
-        origin_grid.PlaceItem(Selected_item, origin_pos);
-        Selected_item = null;
+        origin_grid.PlaceItem(selected_item, origin_pos);
         tile_offset = Vector2Int.zero;
+    }
+
+    InvItem to_equip_item;
+    private void EquipItem(){
+        Vector2Int mouse_grid_pos = selected_item_grid.GetGridPos(Input.mousePosition) + tile_offset;
+        to_equip_item = selected_item_grid.GetItem(mouse_grid_pos);
+        if(to_equip_item.item_data.equippable){
+            if(to_equip_item == equipped_item){
+            inv_highlighter.SetVisible(1, false);
+            equipped_item = null;
+            HandleHighlight(false);
+            Debug.Log("item unequipped");
+        }
+        else if(to_equip_item != null){
+            equipped_item = to_equip_item;
+            inv_highlighter.SetSize(1, equipped_item, selected_item_grid);
+            inv_highlighter.SetParent(1, selected_item_grid);
+            inv_highlighter.SetPosition(1, selected_item_grid, equipped_item);
+            inv_highlighter.SetVisible(1, true);
+            inv_highlighter.SetVisible(0, false);
+            Debug.Log("item equipped");
+        }
+        }
     }
 }
--- Assets/Scripts/Inventory/ItemData.cs
+++ Assets/Scripts/Inventory/ItemData.cs
@@ -1,26 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
+
 [CreateAssetMenu]
 public class ItemData : ScriptableObject
 {
-
     
-    [Serializable]
-    public struct GunStats{
-        public int fire_rate;
-        public int dammage;
-        public int recoil;
-    }
-    
-    public int width = 1;
-    public int height = 1;
-
+    public int width;
+    public int height;
+    public bool stackable;
+    public bool equippable;
     public Sprite item_icon;
-
-    public int item_type = 1;
-    public GunStats gunStats;
+    public ItemType item_type;
+    
 }
