0ddedb9cd040f05225f4f7f2dc6ac923cf2cbcb2
Rnguyen2019 <rnguyen2019@student.pps.net>
2024-02-29 11:40:19 -0800

    Added ruins and ruin spawning

---
+++ Assets/Scripts/Ryan Scripts/RuinVisbility.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuinVisbility : MonoBehaviour
+{
+    public GameObject[] Wall1;
+    public GameObject[] Wall2;
+    public GameObject[] Wall3;
+    public GameObject[] Wall4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < 2; x++)
+        {
+            Wall2[x].SetActive(false);
+            Wall3[x].SetActive(false);
+            Wall4[x].SetActive(false);
+        }
+        Wall2[Random.Range(0, 2)].SetActive(true);
+        Wall3[Random.Range(0, 2)].SetActive(true);
+        Wall4[Random.Range(0, 2)].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
