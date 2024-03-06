58b9cabdae71883a3babc650e6dbf12b3a3cf257
Rnguyen2019 <rnguyen2019@student.pps.net>
2024-03-06 10:56:27 -0800

    Added shotgun

--- Assets/Scripts/Ryan Scripts/GunSelectVisibility.cs
+++ Assets/Scripts/Ryan Scripts/GunSelectVisibility.cs
@@ -6,6 +6,7 @@ public class GunSelectVisibility : MonoBehaviour
 {
     public GameObject[] tools;
     public Animator animator;
+    public bool seeEquippedItem;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +20,10 @@ public class GunSelectVisibility : MonoBehaviour
     void Update()
     {
         if (InvController.equipped_item != null){
-            //Debug.Log(InvController.equipped_item.item_data.item_type);
+            if (seeEquippedItem)
+            {
+                Debug.Log(InvController.equipped_item.item_data.name);
+            }
             for (int i = 0; i <= tools.Length - 1; i++){
                 if (InvController.equipped_item.item_data.name.Equals(tools[i].name)){
                     tools[i].SetActive(true);
