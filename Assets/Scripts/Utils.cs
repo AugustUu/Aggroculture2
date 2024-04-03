1c794329fbbc9411568f9ba457324912da134a96
Rnguyen2019 <rnguyen2019@student.pps.net>
2024-04-03 11:55:19 -0700

    Revert "Revert "Merge branch 'main' into Character-Branch""
    This reverts commit 585136a6fa4935a86ae9d1190d63f09d0fda5530.

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -6,24 +6,30 @@ using UnityEngine;
 
 public class Utils : MonoBehaviour
 {
-    public static Dictionary<SeedType, int> farm_dict =
-    new Dictionary<SeedType, int>{//seedtype, itemid
-        {SeedType.Carrot, 0},
-        {SeedType.Potato, 3}
-    };
+    [SerializeField]
+    public List<PlantData> plant_list_init;
+    public static List<PlantData> plant_list;
+    
+    void Start(){
+        plant_list = plant_list_init;
+    }
 }
 
 public enum ItemList{
     schmeeze,
-    eeee,
-    aaaa,
-    big,
     gozoman,
-    stockedAK,
+    tomato_seeds,
+    potato_seeds,
+    carrot_seeds,
+    tomato,
+    potato,
+    carrot,
     shoel,
+    stockedAK,
     stocklessAK,
     oldGun,
     glock,
     shotgun,
     smg
+    
 }
